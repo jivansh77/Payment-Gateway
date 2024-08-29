5badac313f3adee9b15ba97701bcbe9e855253e7
jivansh77 <jivansh777@gmail.com>
2024-08-29 17:58:18 +0530

    add files

---
+++ app.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const app = require('express')();
+var http = require('http').Server(app);
+
+const paymentRoute = require('./routes/paymentRoute');
+
+app.use('/donation',paymentRoute);
+
+http.listen(3000, function(){
+    console.log('Server is running');
+});
